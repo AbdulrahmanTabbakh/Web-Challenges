66a05200cbefc8e4eae026901481042145190e48
Abdulrahman Tabbakh <a.r.tabbakh@gmail.com>
2024-05-10 08:57:50 +0200

    add solution

---
+++ js-structure_shapes/index.js
@@ -0,0 +1,20 @@
+console.clear();
+import { getRandomColor } from "./utils/randomColor.js";
+import { Circle } from "./components/Circle/Circle.js";
+
+const root = document.getElementById("root");
+
+const square = document.createElement("div");
+square.classList.add("square");
+square.addEventListener("click", () => {
+  square.style.backgroundColor = getRandomColor();
+});
+
+const pentagon = document.createElement("div");
+pentagon.classList.add("pentagon");
+pentagon.addEventListener("click", () => {
+  pentagon.style.backgroundColor = getRandomColor();
+});
+
+const circleElement = Circle();
+root.append(circleElement, square, pentagon);
